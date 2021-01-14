2162f49578ab3bc751d7de493e2f8ca6e4cb1067
Kuka <martinelliluca@gmail.com>
2021-01-14 11:18:31 -0300

    agregue data sobre personas

---
+++ data.js
@@ -0,0 +1,43 @@
+NamePerson = ['Jonh', 'Ryan', 'Billy', 'Johan'];
+
+
+//Data de las personas
+
+
+Jonh = { 
+    'name': 'Jonh',
+    'userName' : 'Parker',
+    'DNI' : '30.244525',
+    'pay' : 10000
+};
+
+//console.log(Jonh);
+
+Ryan = {
+  'name': 'Ryan',
+  'userName' : 'Parker',
+  'DNI' : '31.546732',
+  'debt' : 500000
+};
+
+//console.log(Ryan);
+
+Billy = {
+  'name': 'Billy',
+  'userName' : 'Marrighy',
+  'DNI' : '29.328562',
+   'debt' : 2000000
+};
+
+//console.log(Billy);
+
+Johan = {
+  'name': 'Johan',
+  'userName' : 'mannnor',
+  'DNI' : '30.243747',
+  'debt' : 300000
+};
+
+//console.log(Johan);
+
+
---
+++ functions.js
@@ -0,0 +1,16 @@
+function add (x1, x2) { 
+	return  x1 + x2;
+		console.log(add);
+}
+
+//console.log(add(5, 5));
+
+
+
+function substract (x1, x2) {
+	return  x1 - x2;
+	console.log(substract);
+}
+
+//console.log(substract(5, 5));
+
