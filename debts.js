1631d6819a52cf11cdde3d8f60cb3ba690bb4177
Kuka <martinelliluca@gmail.com>
2021-01-14 12:07:06 -0300

    agregure la paga de Jonh

---
+++ debts.js
@@ -0,0 +1,27 @@
+Jonh = { 
+  'name': 'Jonh',
+  'userName' : 'Parker',
+  'DNI' : '30.244525',
+  'pay' : 10000
+};
+
+console.log(Jonh);
+
+Jonh = 10000
+
+
+//Funcion para sumar
+
+
+function add (x1, x2) { 
+	return  x1 + x2;
+		//console.log(add);
+}
+
+//cobra la deuda
+
+console.log('depositite : $10000 Now you have :')
+
+	console.log(add(500000, 10000));
+
+ 		let addition = Jonh + add;
